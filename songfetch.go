0257562a1b01c16674d579bceb1d1fd19d39e765
Milos Knezevic <ushtipak@gmail.com>
2018-02-18 00:33:53 +0100

    Nice, I'm happier :)

--- songfetch.go
+++ songfetch.go
@@ -15,6 +15,7 @@ import (
 	"os"
 	"encoding/json"
 	"os/exec"
+	"io"
 )
 
 var (
@@ -36,68 +37,88 @@ type YtResponse struct {
 var client = &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}}
 var wg sync.WaitGroup
 
-func checkError(err error) {
+// verifyStep checks for errors in execution, updates output accordingly and exists if needed
+func verifyStep(err error) {
 	if err != nil {
+		fmt.Println("FAIL")
 		log.Fatal(err)
+	} else {
+		fmt.Println("done")
 	}
 }
 
-// UPDATE ME XD
-func getSongsFromImage(img string) {
-	defer log.Println("all done \\o/")
+// prepareForFetch creates output dir and fetches image to source-img
+// it returns output dir string
+func prepareForFetch(img string) string {
+	outputDir = "songfetch" + time.Now().Format("--2006-01-02--15-04-05--") + strings.ToLower(reg.ReplaceAllString(img, "-"))
+
+	fmt.Print("> create output dir ... ")
+	err := os.Mkdir(outputDir, 0777)
+	verifyStep(err)
+
+	fmt.Print("> retrieve img content ... ")
+	resp, err := http.Get(img)
+	verifyStep(err)
+	defer resp.Body.Close()
+
+	fmt.Print("> create fd ... ")
+	f, err := os.Create(outputDir + "/source-img")
+	verifyStep(err)
+	defer f.Close()
+
+	fmt.Print("> write img to file ... ")
+	_, err = io.Copy(f, resp.Body)
+	verifyStep(err)
+
+	return outputDir
+}
+
+// getSongsFromImage does ocr on fetched img and calls fetchSong on each line of text with target delimiter
+func getSongsFromImage(outputDir string) {
+	defer fmt.Println("> all done \\o/")
+	defer fmt.Println(" done")
 	defer wg.Wait()
 
+	fmt.Print("> perform img ocr ... ")
 	client := gosseract.NewClient()
 	defer client.Close()
-	client.SetImage("./test.png")
+	client.SetImage(outputDir + "/source-img")
 	text, err := client.Text()
-	checkError(err)
+	verifyStep(err)
 
-	err = os.Mkdir(outputDir, 0777)
-	checkError(err)
-
+	fmt.Print("> processing ")
 	songs := strings.Split(text, "\n")
 	for _, song := range songs {
-		log.Printf("=> [%v]", song)
 		if strings.Contains(song, *delimiter) {
-			log.Printf("=> [%v] : YES", song)
 			wg.Add(1)
-			go fetchSong(song)
+			go fetchSong(song, outputDir)
 		}
 	}
 }
 
-// UPDATE ME XD
-func fetchSong(song string) {
-	defer log.Println(strings.ToLower(song))
+// fetchSong retrieves song youtube video id via yt api and downloads it with local youtube-dl cli tool
+func fetchSong(song, outputDir string) {
+	defer fmt.Print(".")
 	defer wg.Done()
 
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://www.googleapis.com/youtube/v3/search?q=%v&maxResults=1&part=snippet&key=AIzaSyCURl1CVR_-gL227h5S8GIhtGXU7kMIFvc", url.QueryEscape(reg.ReplaceAllString(song, ""))), nil)
-	if err != nil {
-		log.Printf("http.NewRequest FAIL [%v]", err)
-	}
-	if response, err := client.Do(req); err == nil {
-		if response.Body != nil {
-			if err := json.NewDecoder(response.Body).Decode(&ytResp); err != nil {
-				log.Printf("json.NewDecoder FAIL [%v]", err)
-			}
-			response.Body.Close()
+	req, _ := http.NewRequest("GET", fmt.Sprintf("https://www.googleapis.com/youtube/v3/search?q=%v&maxResults=1&part=snippet&key=AIzaSyCURl1CVR_-gL227h5S8GIhtGXU7kMIFvc", url.QueryEscape(reg.ReplaceAllString(song, ""))), nil)
+	if resp, err := client.Do(req); err == nil {
+		if resp.Body != nil {
+			json.NewDecoder(resp.Body).Decode(&ytResp)
+			resp.Body.Close()
 		}
-	} else {
-		log.Printf("client.Do FAIL [%v]", err)
 	}
+
 	id := ytResp.Items[0].ID.VideoID
 	if id != "" {
 		cmd := exec.Command("youtube-dl", "-x", "--audio-format", "mp3", fmt.Sprintf("https://www.youtube.com/watch?v=%v", id))
 		cmd.Dir = outputDir
-		if _, err := cmd.CombinedOutput(); err != nil {
-			log.Printf("exec.Command FAIL [%v]", err)
-		}
+		cmd.CombinedOutput()
 	}
 }
 
 func main() {
 	flag.Parse()
-	outputDir = "songfetch" + time.Now().Format("--2006-01-02--15-04-05--") + strings.ToLower(reg.ReplaceAllString(*img, "-"))
-	getSongsFromImage(*img)
+	outputDir := prepareForFetch(*img)
+	getSongsFromImage(outputDir)
 }
